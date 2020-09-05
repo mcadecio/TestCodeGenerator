0b3d443e0184271ce5d3507bd06e25e389aabc24
Dercio Daio <daio.dercio2015@gmail.com>
2020-09-05 17:17:53 +0100

    Added favicon.ico

--- src/test/java/com/ddaio/testcodegen/generator/testcode/random/RandomLoginTestCodeGeneratorTest.java
+++ src/test/java/com/ddaio/testcodegen/generator/testcode/random/RandomLoginTestCodeGeneratorTest.java
@@ -42,7 +42,7 @@ class RandomLoginTestCodeGeneratorTest {
         assertEquals(10, result.getQuantity());
         assertEquals(10, result.getTestCodes().size());
     }
-    
+
 
     @Test
     @DisplayName("Generates 0 test codes when the quantity is negative")
